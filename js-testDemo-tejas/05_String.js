4029527abc06ea6529b402511c80dc810b74ea7f
tejaskhairkar17 <tejaskhairkar@gmail.com>
2026-01-28 00:21:13 +0530

    new string ops

--- js-testDemo-tejas/05_String.js
+++ js-testDemo-tejas/05_String.js
@@ -1,10 +1,10 @@
-let s = new String("This is Tejas");
-let rev =""
-for(let i = s.length -1 ; i>=0; i--){
-    rev += s.charAt(i);
-}
-console.log(rev);
-console.log(`Length of Striing is : ${slength}`);
+// let s = new String("This is Tejas");
+// let rev =""
+// for(let i = s.length -1 ; i>=0; i--){
+//     rev += s.charAt(i);
+// }
+// console.log(rev);
+// console.log(`Length of Striing is : ${s.length}`);
 
 // // String is a sequence of characters
 // // You can access each character using charAt() method
@@ -26,3 +26,30 @@ console.log(`Length of Striing is : ${slength}`);
 // // Strings are widely used in JS for various purposes like displaying messages, taking input, etc.
 // // // Always remember to use string methods to manipulate strings effectively
 // // // Happy Coding!!!
+
+// String operations
+let str1 = "Hello";
+let str2 = "World"; 
+let str3 = str1 + " " + str2; // Concatenation
+console.log("Concatenated String:", str3);
+
+let str4 = `${str1}, ${str2}!`; // Template Literal
+console.log("Template Literal String:", str4);
+
+let str5 = str3.slice(0, 5); // Slicing
+console.log("Sliced String:", str5);
+
+let str6 = str3.replace("World", "JavaScript"); // Replacement
+console.log("Replaced String:", str6);
+
+// // String operations are essential for manipulating text data in JS
+// // Concatenation is done using + operator or template literals
+// // Slicing is done using slice() method which takes start and end indices
+// // Replacement is done using replace() method which takes the substring to be replaced and the new substring
+// // There are many more string operations available in JS
+// // You can refer to MDN documentation for more details on string operations
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// // Strings are widely used in JS for various purposes like displaying messages, taking input, etc.
+// // Always remember to use string methods to manipulate strings effectively
+// // Happy Coding!!!
+
